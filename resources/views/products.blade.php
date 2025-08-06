f8d51fcc1f47d2c9401c712ada0fbb016a061ee3
Thiennguyen012 <thiennguyen5432@gmail.com>
2025-08-06 16:42:21 +0700

    Fix Vite manifest error by removing products.js dependency

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -6,10 +6,6 @@
     </h2>
 @endsection
 
-@push('scripts')
-    @vite(['resources/js/products.js'])
-@endpush
-
 @section('products-content')
     <!-- Products Grid -->
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
